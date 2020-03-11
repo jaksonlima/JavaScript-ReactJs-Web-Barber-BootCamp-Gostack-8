84a617fd38265935250ad01510e1d8d8e6dcc915
jaksonsneider <jaksonsneider@gmail.com>
2020-03-10 23:19:37 -0300

    image profile

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import { Link } from "react-router-dom";
 import { useSelector } from "react-redux";
 
@@ -6,8 +6,24 @@ import Notifications from "../Notifications/index";
 
 import logoPurple from "../../assets/logo-purple.svg";
 import { Container, Content, Profile } from "./styles";
+import api from "../../services/api";
 
 export default function Header() {
+  const [image, setImage] = useState("");
+
+  const { id } = useSelector(state => state.user.profile);
+
+  useEffect(() => {
+    async function handleImageUser() {
+      const reponse = await api.get(`files/?id_user=${id}`);
+      const { url_avatar } = reponse.data.find(avatar => avatar.id_user === id);
+
+      setImage(url_avatar);
+    }
+
+    handleImageUser();
+  }, [id]);
+
   return (
     <>
       <Container>
@@ -25,10 +41,7 @@ export default function Header() {
                 <strong>Jakson Lima</strong>
                 <Link to="/profile">Meu Perfil</Link>
               </div>
-              <img
-                src="https://scontent.fcac3-1.fna.fbcdn.net/v/t1.0-9/p960x960/80386294_2601296719988893_8250196507018395648_o.jpg?_nc_cat=102&_nc_sid=dd7718&_nc_ohc=K6tVcInKUn4AX8DURnb&_nc_ht=scontent.fcac3-1.fna&_nc_tp=6&oh=83d99d9bfdb75e4c315dd0ab5fb88255&oe=5E947731"
-                alt="fack"
-              />
+              <img src={image} alt="imageFack" />
             </Profile>
           </aside>
         </Content>

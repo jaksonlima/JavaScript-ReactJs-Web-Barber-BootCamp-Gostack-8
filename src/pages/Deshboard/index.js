0251c5bf87adee9930a5ea7a7a853f86469d570b
jaksonsneider <jaksonsneider@gmail.com>
2020-03-12 23:35:41 -0300

    Deshboard

--- src/pages/Deshboard/index.js
+++ src/pages/Deshboard/index.js
@@ -1,7 +1,63 @@
-import React from "react";
+import React, { useState, useMemo } from "react";
+import { format, subDays, addDays } from "date-fns";
+import pt from "date-fns/locale/pt";
+import { MdChevronLeft, MdChevronRight } from "react-icons/md";
 
-import { Container } from "./styles";
+import { Container, Time } from "./styles";
 
 export default function Deshboard() {
-  return <Container>Deshboard</Container>;
+  const [date, setDate] = useState(new Date());
+
+  const dataFormatted = useMemo(
+    () => format(date, "d 'de' MMMM", { locale: pt }),
+    [date]
+  );
+
+  function handlePrevDay() {
+    setDate(subDays(date, 1));
+  }
+
+  function handleNextDay() {
+    setDate(addDays(date, 1));
+  }
+
+  return (
+    <Container>
+      <header>
+        <button type="button" onClick={handlePrevDay}>
+          <MdChevronLeft size={36} color="#fff" />
+        </button>
+        <strong>{dataFormatted}</strong>
+        <button type="button" onClick={handleNextDay}>
+          <MdChevronRight size={36} color="#fff" />
+        </button>
+      </header>
+      <ul>
+        <Time past>
+          <strong>08:00</strong>
+          <span>Jakson Lima</span>
+        </Time>
+        <Time available>
+          <strong>08:00</strong>
+          <span>Jakson Lima</span>
+        </Time>
+        <Time>
+          <strong>08:00</strong>
+          <span>Jakson Lima</span>
+        </Time>
+        <Time>
+          <strong>08:00</strong>
+          <span>Jakson Lima</span>
+        </Time>
+        <Time>
+          <strong>08:00</strong>
+          <span>Jakson Lima</span>
+        </Time>
+        <Time>
+          <strong>08:00</strong>
+          <span>Jakson Lima</span>
+        </Time>
+      </ul>
+    </Container>
+  );
 }
--- src/pages/Deshboard/styles.js
+++ src/pages/Deshboard/styles.js
@@ -1,5 +1,64 @@
 import styled from "styled-components";
 
 export const Container = styled.div`
-  background: linear-gradient(-90deg, red, yellow);
+  /* background: linear-gradient(-90deg, red, yellow); */
+  width: 600px;
+  margin: 50px auto;
+
+  display: flex;
+  flex-direction: column;
+
+  header {
+    display: flex;
+    align-items: center;
+    justify-content: center;
+
+    button {
+      border: 0;
+      background: none;
+    }
+
+    button:hover {
+      opacity: 0.5;
+    }
+
+    strong {
+      color: #fff;
+      font-size: 24px;
+      margin: 0 15px;
+    }
+  }
+
+  ul {
+    display: grid;
+    grid-template-columns: repeat(2, 1fr);
+    grid-gap: 15px;
+    margin-top: 30px;
+  }
+`;
+
+export const Time = styled.div`
+  padding: 20px;
+  border-radius: 5px;
+  background: #fff;
+
+  opacity: ${props => (props.past ? 0.6 : 1)};
+
+  strong:hover {
+    opacity: 0.5;
+    color: red;
+  }
+
+  strong {
+    display: block;
+    color: ${props => (props.available ? "#999" : "#7159c1")};
+    font-size: 20px;
+    font-weight: normal;
+  }
+
+  span {
+    display: block;
+    margin-top: 3px;
+    color: ${props => (props.available ? "#999" : "#666")};
+  }
 `;
